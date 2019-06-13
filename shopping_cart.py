9a4e8782f788ebce4f9a8db5b57ed76809b41775
ktg269 <50781186+ktg269@users.noreply.github.com>
2019-06-13 18:11:43 -0400

    Lookup Validations

--- shopping_cart.py
+++ shopping_cart.py
@@ -27,20 +27,27 @@ products = [
 
 # INFO CAPTURE/INPUT
 
+valid_ids = [str(p["id"]) for p in products] # doing comparisons with string versions of these ids
+#print("VALID IDS:", valid_ids)
+
 total_price = 0
 selected_ids = []
 
 while True:
-    selected_id = input("Please input a product identifier: ") #> "9" (string)
+    selected_id = input("Please input a product identifier, or DONE: ") #> "9" (string)
     #> "DONE"
     if selected_id == "DONE":
-        break
+        break # stops the loop
+    elif str(selected_id) in valid_ids:
+        selected_ids.append(selected_id)
     else:
-            #matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-            #matching_product = matching_products[0]
-            #total_price = total_price + matching_product["price"]
-            #print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
-            selected_ids.append(selected_id)
+        print("Invalid input. Please try again...")
+            
+        #matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        #matching_product = matching_products[0]
+        #total_price = total_price + matching_product["price"]
+        #print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+        #selected_ids.append(selected_id)
 
 # INFO DISPLAY/OUTPUT
 
