bcd6ae65b85c1971ae31dd0fcf47ae211bfa0f42
ktg269 <50781186+ktg269@users.noreply.github.com>
2019-06-13 19:47:30 -0400

    Dollars and Cents Formatting

--- shopping_cart.py
+++ shopping_cart.py
@@ -62,29 +62,34 @@ print("WWW.BESTINTOWNGROCERY.COM")
 print("------------------------------")
 
 # The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2019-06-06 11:31 AM)
-# Referece: The datetime Module guide (office Python guide)
+# Referece: The datetime Module guide (official Python guide)
 
 from datetime import datetime
 t = datetime.today()
 print("CHECKOUT AT: " + t.strftime("%Y-%m-%d %I:%M %p"))
+print("------------------------------")
 
 
+# The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $1.50)
 
-# print(selected_ids)
+def usd(store_price):
+    return '${:,.2f}'.format(store_price)
+
+print("SELECTED PRODUCT(S): ")
 
 for selected_id in selected_ids:
         matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
         matching_product = matching_products[0]
         total_price = total_price + matching_product["price"]
-        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+        print("... "  + matching_product["name"] + " (" + usd(matching_product["price"])+")")
 
 
 
-print("TOTAL PRICE: " + str(total_price)) ## format as USD
+print("TOTAL PRICE: " + usd(total_price)) ## format as USD
 
 
 
-# The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $1.50)
+
 # The total cost of all shopping cart items, formatted as US dollars and cents (e.g. $4.50), calculated as the sum of their prices
 # The amount of tax owed (e.g. $0.39), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
 # The total amount owed, formatted as US dollars and cents (e.g. $4.89), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
